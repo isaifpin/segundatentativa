2d7b8412288008b154f4dab46371767155744d5b
isaifpin <isabellebraz2401@gmail.com>
2019-08-22 14:30:32 -0300

    P3

--- js.js
+++ js.js
@@ -1,34 +1,36 @@
+jQuery.support.cors = true;
 
-city_name = "Delmiro Gouveia";
+var data;
+var statusMap;
 
-function requestWeather(city_name) {
+const requestWeather = () => {
+    let cidade = document.getElementById("cidade").value;
   
    $.getJSON({
        url: "https://api.hgbrasil.com/weather",
        data: {
-           key: '57956d7b',
+           key: '6ee295f6',
            format: 'json-cors',
-           woeid: 459329,
-           locale: "pt-br"
+           city_name: cidade
         },
        success: function ( result ) {
+           if (result.by == "default") {
+               alert("Cidade não encontrada!")
+               
+            }
+
+        else{
 
            data = result.results;
            city_name = data.city_name;
 
-            $ ("#cidade").html ("Clima em: " + city_name );
+            $ ("#idcidade").html ("Clima em: " + data.city_name );
             $ ("#temp").html ("Temperatura atual: " + data.temp + "°C"); 
             $ ("#status").html ("Descrição: " + data.description);
             $ ("#vent").html ("Velocidade do vento: " + data.wind_speedy);
             $ ("#nascer").html ("Nascer do sol: " + data.sunrise);
-            $ ("#porsol").html (" Por do sol: " + data.sunset);
-          
+            $ ("#porsol").html (" Por do sol: " + data.sunset);        }
         }
     })
 }
-$( document ).ready(function() {
-
-    
-    requestWeather(city_name)
-});
    
